293cb4a0bf1ae10783ae339e23f97e9cc6c8c6b7
Rezzubs <marten.roots@gmail.com>
2019-12-03 18:42:38 +0200

    kombinatsiooniblockid

--- src/components/Combination/Combination.js
+++ src/components/Combination/Combination.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Combination.css'
+import C from './C.png'
 
 class Combination extends React.Component {
     render() {
         return (
             <div className="container">
                 <div className="arvutus">
+                    <img className="C"></img>
                     arvutus
                 </div>
                 <div className="vastus">vastus</div>
