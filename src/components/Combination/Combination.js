f919b69d48a3b2fa24de7c1c582a95e6152ef91f
Rezzubs <marten.roots@gmail.com>
2019-12-04 12:58:29 +0200

    input works now :)

--- src/components/Combination/Combination.js
+++ src/components/Combination/Combination.js
@@ -2,8 +2,32 @@ import React from 'react';
 import './Combination.css'
 import C from './C.png'
 import equals from './Equals.png'
+import { Logic } from './Logic'
+
+
 
 class Combination extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            awnser: '',
+            upperValue: '',
+            lowerValue: ''
+        };
+
+        //binds
+        this.handleUpperChange = this.handleUpperChange.bind(this);
+        this.handleLowerChange = this.handleLowerChange.bind(this);
+    }
+
+    handleUpperChange(e) {
+        this.setState({ upperValue: e.target.value })
+    }
+
+    handleLowerChange(e) {
+        this.setState({ lowerValue: e.target.value })
+    }
+    
     render() {
         return (
             <div className="container">
@@ -12,19 +36,21 @@ class Combination extends React.Component {
                         <img src={C}></img>
                     </div>
                     <div className="input">
-                        <input className="upper"></input>
-                        <input className="lower"></input>
+                        <input className="upper"  onChange={this.handleUpperChange}></input>
+                        <input className="lower"  onChange={this.handleLowerChange}></input>
                     </div>
                     <div className="equals">
                         <img src={equals}></img>
                     </div>
                     <div className="awnser">
-                        <h1>4732489351889215475643575646</h1>
+                        <h1>{this.state.lowerValue}</h1>
                     </div>
                 </div>
             </div>
         )
     }
 }
+
+
 
 export default Combination;
