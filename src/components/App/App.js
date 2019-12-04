2bc47deef38fce993889199c6ebe815486d81274
Rezzubs <marten.roots@gmail.com>
2019-12-04 11:59:11 +0200

    more advanced grid layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="navbar">maagiline navbar</div>
-      <div className="container">
-        <Combination className="combination" />
-      </div>
+      <Combination />
     </div>
   );
 }
--- src/components/Combination/Combination.js
+++ src/components/Combination/Combination.js
@@ -5,7 +5,20 @@ import C from './C.png'
 class Combination extends React.Component {
     render() {
         return (
-            <div className="combinationContainer">
+            <div className="container">
+                <div className="combination">
+                    <div className="CImg">
+                        <img src={C}></img>
+                    </div>
+                    <div className="input">
+                        <input className="upper"></input>
+                        <input className="lower"></input>
+                    </div>
+                    <div className="equals">
+                        
+                    </div>
+                    <div className="awnser">20503</div>
+                </div>
             </div>
         )
     }
